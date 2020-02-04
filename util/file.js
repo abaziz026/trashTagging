a1dfbdd9fc7c9bf2c204ac9e101aa2a220ea6a3e
Abdul Aziz <abaziz026@gmail.com>
2020-02-04 13:18:09 +0500

    added aziz's unlink implementation

--- util/file.js
+++ util/file.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 const deleteFile = filePath => {
   fs.unlink(filePath, err => {
     if (err) {
-      throw err;
+      if (err.errno === -4058) return true;
+    } else {
+      console.error(err);
     }
   });
 };
-
 exports.deleteFile = deleteFile;
